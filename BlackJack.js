9de5f78c591cd0f06a7456b7381cc33062dc77a4
Gagulo <grzegorz.sokalski@hotmail.com>
2018-10-07 17:11:53 +0200

    Adding Object

--- BlackJack.js
+++ BlackJack.js
@@ -1,16 +1,24 @@
-let colors = ['Diamonds', 'Hearts', 'Clubs', 'Spades'];
-let values = ['Ace', 'King', 'Queenie', 'Jack', 
+let color = ['Diamonds', 'Hearts', 'Clubs', 'Spades'];
+let value = ['Ace', 'King', 'Queenie', 'Jack', 
             'Ten', 'Nine', 'Eight', 'Seven', 
             'Six', 'Five', 'Four', 'Three', 'Two'];
 
 function createDeck(){
     let deck = [];
-    for (let colorsIdx = 0; colorsIdx < colorsIdx.length; colorsIdx++){
-        for (let valuesIdx = 0; valuesIdx < valuesIdx.length; valuesIdx++){
-            deck.push(values[valuesIdx] + ' of ' + colors[colorsIdx]);
+    for (let colorIdx = 0; colorIdx < colorIdx.length; colorIdx++){
+        for (let valueIdx = 0; valueIdx < valueIdx.length; valueIdx++){
+            let card = {
+                color: color[colorIdx],
+                value: value[valueIdx]
+            };
+            deck.push(card);
         }
     }
     return deck;
+}
+
+function CardString(card){
+    return card.value + ' of ' + card.color;
 }
 
 function NextCard(){
