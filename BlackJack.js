7546d34beaa0c243b0e342ae97fbe340e44ccaa6
Gagulo <grzegorz.sokalski@hotmail.com>
2018-10-07 17:03:59 +0200

    adding functions

--- BlackJack.js
+++ BlackJack.js
@@ -2,13 +2,20 @@ let colors = ['Diamonds', 'Hearts', 'Clubs', 'Spades'];
 let values = ['Ace', 'King', 'Queenie', 'Jack', 
             'Ten', 'Nine', 'Eight', 'Seven', 
             'Six', 'Five', 'Four', 'Three', 'Two'];
-            
-let deck = [];
 
-for (let colorsIdx = 0; colorsIdx < colorsIdx.length; colorsIdx++)
-{
-    for (let valuesIdx = 0; valuesIdx < valuesIdx.length; valuesIdx++)
-    {
-        deck.push(values[valuesIdx] + ' of ' + colors[colorsIdx]);
+function createDeck(){
+    let deck = [];
+    for (let colorsIdx = 0; colorsIdx < colorsIdx.length; colorsIdx++){
+        for (let valuesIdx = 0; valuesIdx < valuesIdx.length; valuesIdx++){
+            deck.push(values[valuesIdx] + ' of ' + colors[colorsIdx]);
+        }
     }
+    return deck;
 }
+
+function NextCard(){
+    return deck.shift();
+}
+
+let deck = createDeck();
+let playerCards = [NextCard(), NextCard()];
